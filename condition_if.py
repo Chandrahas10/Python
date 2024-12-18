39307603dede209f96949644c9b56b22d3617d5e
Chandrahas10 <142862718+Chandrahas10@users.noreply.github.com>
2024-12-18 18:46:42 +0530

    conditional expression
    all conditional expression are covered if and while condition

---
+++ condition_if.py
@@ -0,0 +1,21 @@
+a=int(input("Enter your age"))
+#if elif else ladder
+if(a>=18):
+
+  print("You are eligible for voting")
+
+elif(a<0):
+    print("You enterd negative number")
+
+elif(a==0):
+    print("You entered 0 ")
+else:
+    print("you are less than 18")
+
+
+#if else 
+a=int(input("enter a number"))
+if(a%2==0):
+  print("Even number")
+else:
+   print("odd number")
---
+++ conditional_practice_set.py
@@ -0,0 +1,20 @@
+# 1. Write a program to find the greatest of four numbers entered by the user.
+# a1=int(input("Enter number 1:"))
+# a2=int(input("Enter number 2:"))
+# a3=int(input("Enter number 3:"))
+# a4=int(input("Enter number 4:"))
+# if(a1>a2 and a1>a3 and a1>a4):
+#   print("a is gretest number")
+
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print("a2 is greatest no.")
+
+# elif(a3>a1 and a3>a2 and a3>a4):
+#     print("a3 is greatest no.")
+
+# else:
+#     print("a4 is greatest no.")
+
+# 2. Write a program to find out whether a student has passed or failed if it requires a 
+# total of 40% and at least 33% in each subject to pass. Assume 3 subjects and 
+# take marks as an input from the user.
